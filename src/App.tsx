03d76c3a42ba72aab93e9ea8e34106a7163bff6a
Mohammed Ennabah <ennabahm9@gmail.com>
2022-04-25 03:15:11 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{ fontSize: 12 }}>ennabah.com</p>
       </header>
+      <body className="Body">
+          <p style={{ fontSize: 14 }}>a new design is coming soon™  but hey, it's dark mode!</p>
+          <div className="Seperator" />
+          <ul>
+            <li>add m@ to this domain to email me</li>
+            <li><a href="https://twitter.com/M_Ennabah" target="_blank">twitter</a></li>
+            <li><a href="https://github.com/MEnnabah" target="_blank">github</a></li>
+            <li><a href="https://www.linkedin.com/in/mohammedennabah/" target="_blank">linkedin</a></li>
+            <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">my youtube channel</a></li>
+          </ul>
+        </body>
     </div>
   );
 }
